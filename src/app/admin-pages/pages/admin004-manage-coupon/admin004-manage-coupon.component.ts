c9de2cdd263197963d4d70a9775d1ee3806729ed
thanhdev <doquocthanh@github.com>
2024-07-06 01:23:31 +0700

    admin-pages - pages/admin004-information-customer - add: Khởi động admin004, test drawer

--- src/app/admin-pages/pages/admin004-manage-coupon/admin004-manage-coupon.component.ts
+++ src/app/admin-pages/pages/admin004-manage-coupon/admin004-manage-coupon.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DrawerMode, DrawerPosition } from '@progress/kendo-angular-layout';
 
 @Component({
   selector: 'app-admin004-manage-coupon',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin004-manage-coupon.component.scss']
 })
 export class Admin004ManageCouponComponent {
-
+  // Chế độ hiển thị của drawer
+  expandMode: DrawerMode = 'overlay';
+  // Drawer đang được mở hay không
+  expanded = false;
+  // Chiều dài của drawer
+  widthDrawer: number = 550;
+  // Vị trị xuất hiện của drawer
+  positionDrawer: DrawerPosition = "end";
 }
