caae088e8f081bddf942648dc7ea7a2222bcdc52
LeHuuMinh0509 <lehuuminhtc31@gmail.com>
2024-07-19 11:08:56 +0700

    .

--- src/app/admin-pages/pages/admin006-manage-cart/admin006-manage-cart.component.ts
+++ src/app/admin-pages/pages/admin006-manage-cart/admin006-manage-cart.component.ts
@@ -257,7 +257,7 @@ export class Admin006ManageCartComponent implements OnInit, OnDestroy {
   }
 
   toLocalString(date: Date, type: string) {
-    const year = date.getFullYear();
+    const year = date?.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
 
